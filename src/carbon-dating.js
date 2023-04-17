4c9010addc9d5bc5cbc7d2602576950bbdcc1c72
kate-rzk <razkovaekaterina@gmail.com>
2023-04-18 02:37:01 +0300

    fix carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,10 +18,12 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-  if (!(sampleActivity instanceof Number)) return false;
-  const k = Math.log(2) / HALF_LIFE_PERIOD;
-  const age = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
-  return age;
+  if (typeof sampleActivity !== 'string' || !Number(sampleActivity) || Number(sampleActivity) <= 0 || Number(sampleActivity) > MODERN_ACTIVITY) {
+    return false;
+  }
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const t = Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / k);
+  return t;
 }
 
 module.exports = {
